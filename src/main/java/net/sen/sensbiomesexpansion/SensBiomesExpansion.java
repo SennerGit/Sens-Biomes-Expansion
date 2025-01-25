6c3027088c9413a723ec507a37796a173c8e51d1
Sen <seangalexander1@gmail.com>
2025-01-25 16:29:13 +1100

    Setting up the mod

---
+++ src/main/java/net/sen/sensbiomesexpansion/SensBiomesExpansion.java
@@ -0,0 +1,43 @@
+package net.sen.sensbiomesexpansion;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+@Mod(SensBiomesExpansion.MODID)
+public class SensBiomesExpansion
+{
+    public static final String MODID = "sensbiomesexpansion";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public SensBiomesExpansion(IEventBus modEventBus, ModContainer modContainer)
+    {
+        modEventBus.addListener(this::commonSetup);
+
+        NeoForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+    }
+
+    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+    }
+}
